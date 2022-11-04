967eb937c31e19aa1cf37ac63375d42b29d09f98
dvpunia <103427665+dvpunia@users.noreply.github.com>
2022-11-04 14:16:29 -0400

    Initial Commit

---
+++ Lecture 7-TabsDemo/Lecture_7_TabsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture_7_TabsDemoApp.swift
+//  Lecture 7-TabsDemo
+//
+//  Created by DV Punia on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct Lecture_7_TabsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
